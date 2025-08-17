69a5ab9681ddea47e7a22fee0b004bb508f9be38
Lara Duek <laradu@edu.hac.ac.il>
2025-08-17 15:27:51 +0300

    add env and logger

---
+++ firewalls_app/config/env.ts
@@ -0,0 +1,25 @@
+import 'server-only';
+import { z } from 'zod';
+
+
+const EnvSchema = z.object({
+  ENV: z.enum(['dev', 'prod'], { error: 'ENV must be dev or prod' }),
+  PORT: z.coerce.number().int().min(1).max(65535),
+  SERVER_URL: z.string().url('SERVER_URL must be a valid URL'),
+});
+
+const envParsed = EnvSchema.parse(process.env);
+
+export const config = {
+  env: envParsed.ENV,               // 'dev' | 'prod'
+  port: envParsed.PORT,             // number
+  serverUrl: envParsed.SERVER_URL,  // string
+} as const;
+
+
+export const APP_NAME = 'firewalls_app' as const;
+export const STRINGS = {
+  serverStarting: `${APP_NAME} starting...`,
+  serverReady: `${APP_NAME} ready`,
+  serverError: `${APP_NAME} failed to start`,
+} as const;
---
+++ firewalls_app/config/logger.ts
@@ -0,0 +1,52 @@
+import 'server-only';
+import { createLogger, format, transports, Logger } from 'winston';
+import { config } from './env';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const logFormat = format.combine(
+  format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
+);
+
+function buildTransports() {
+    //dev
+    if (config.env !== 'prod') {
+        return [new transports.Console({ format: format.combine(format.colorize(), logFormat) })];
+    }
+    //prod
+    const logsDir = path.resolve(process.cwd(), 'logs');
+
+    if (!fs.existsSync(logsDir)) {
+        fs.mkdirSync(logsDir);
+    }
+    return [new transports.File({ filename: path.join(logsDir, 'app.log') })];
+}
+
+class AppLogger {
+  private static instance: Logger;
+  private constructor() {}
+
+  public static getInstance(): Logger {
+    if (!AppLogger.instance) {
+      AppLogger.instance = createLogger({
+        level: config.env === 'prod' ? 'info' : 'debug',
+        format: logFormat,
+        transports: buildTransports(),
+        exitOnError: false
+      });
+      if (!(logger as any).__consolePatched) {
+        console.log = (...args) => logger.info(args.join(' '));
+        console.error = (...args) => logger.error(args.join(' '));
+        console.warn = (...args) => logger.warn(args.join(' '));
+        console.debug = (...args) => logger.debug(args.join(' '));
+        (logger as any).__consolePatched = true;
+      }
+      AppLogger.instance = logger;
+    }
+    return AppLogger.instance;
+  }
+}
+
+export const logger = AppLogger.getInstance();
+export default logger;
